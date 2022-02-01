ca40c188b5277379a50c7e96238989bab1eb9128
Salvador HM <salvadorhm@gmail.com>
2022-02-01 16:22:03 +0000

    condicionales

---
+++ programa_09.py
@@ -0,0 +1,26 @@
+foo = 10
+bar = 10
+
+if foo == bar:
+    print("Iguales")
+print("Fin del programa")
+
+foo = 10
+bar = 5
+
+if foo == bar:
+    print("Iguales")
+print("Fin del programa")
+
+foo = 10
+bar = 5
+
+if foo == bar:
+    pass
+
+print("Iguales")
+print("Fin del programa")
+
+if foo == bar:
+    print("Iguales")
+    print("Fin del programa")
